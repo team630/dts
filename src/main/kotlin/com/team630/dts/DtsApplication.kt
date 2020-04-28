2b012d7c78b23bb5014c394c97543156dd0dec1e
sinwoobang <sinwoobang@gmail.com>
2020-04-28 18:32:28 +0900

    Setup project

---
+++ src/main/kotlin/com/team630/dts/DtsApplication.kt
@@ -0,0 +1,11 @@
+package com.team630.dts
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DtsApplication
+
+fun main(args: Array<String>) {
+	runApplication<DtsApplication>(*args)
+}
---
+++ src/test/kotlin/com/team630/dts/DtsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.team630.dts
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DtsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
